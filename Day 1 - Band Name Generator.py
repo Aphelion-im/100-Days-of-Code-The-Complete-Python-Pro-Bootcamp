26fbbd991b04f98bd6e19caa44e9ad3bf06d6007
Aphelion Web Development <17068516+Aphelion-im@users.noreply.github.com>
2025-01-27 09:43:58 +0100

    Add files via upload

---
+++ Day 1 - Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator, user!")
+nameOfCity = input("What is the name of the city you grew up in?: \n")
+nameOfPet = input("What is the name of your pet?: \n")
+print("Your band name is: " + nameOfCity + " " + nameOfPet)
