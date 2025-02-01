bc7fb2f7753d8f4d6f03e8f5342b669416be930e
Aphelion-im <aphelion-im@protonmail.com>
2025-02-01 23:16:24 +0100

    Changed filename day 1

--- Day 1 - Band Name Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Band Name Generator, user!")
-nameOfCity = input("What is the name of the city you grew up in?: \n")
-nameOfPet = input("What is the name of your pet?: \n")
-print("Your band name is: " + nameOfCity + " " + nameOfPet)
